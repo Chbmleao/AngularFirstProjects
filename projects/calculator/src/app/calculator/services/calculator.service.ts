d1113754178bff2a8411e26e2bf46abdb5dbcfdc
Carlos Leão <caikleao@gmail.com>
2023-08-19 00:21:01 -0300

    Implement calculator service

--- projects/calculator/src/app/calculator/services/calculator.service.ts
+++ projects/calculator/src/app/calculator/services/calculator.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculatorService {
+  static readonly PLUS: string = '+';
+  static readonly MINUS: string = '-';
+  static readonly DIVIDE: string = '/';
+  static readonly TIMES: string = '*';
 
-  constructor() { }
+  constructor() {}
+
+  calculate(num1: number, num2: number, operation: string): number {
+    let result: number;
+
+    switch (operation) {
+      case CalculatorService.PLUS:
+        result = num1 + num2;
+        break;
+      case CalculatorService.MINUS:
+        result = num1 - num2;
+        break;
+      case CalculatorService.DIVIDE:
+        result = num1 / num2;
+        break;
+      case CalculatorService.TIMES:
+        result = num1 * num2;
+        break;
+      default:
+        result = 0;
+    }
+
+    return result;
+  }
 }
