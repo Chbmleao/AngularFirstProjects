7c5f26238597129c8271ca98e89aa2aa2db2bae1
Carlos Leão <caikleao@gmail.com>
2023-08-19 00:02:31 -0300

    Add calculator service

--- projects/calculator/src/app/calculator/components/calculator.component.ts
+++ projects/calculator/src/app/calculator/components/calculator.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { CalculatorService } from '../services';
+
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+  styleUrls: ['./calculator.component.css'],
 })
 export class CalculatorComponent {
-
+  constructor(private calculatorService: CalculatorService) {}
 }
