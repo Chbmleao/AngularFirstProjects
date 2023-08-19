7dd896ddbb44c9014db27530233873f152942fef
Carlos Leão <caikleao@gmail.com>
2023-08-19 00:40:40 -0300

    Testing calculator service

--- projects/calculator/src/app/app.component.spec.ts
+++ projects/calculator/src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { CalculatorModule } from './calculator';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [CalculatorModule],
     })
   );
 
--- projects/calculator/src/app/calculator/services/calculator.service.spec.ts
+++ projects/calculator/src/app/calculator/services/calculator.service.spec.ts
@@ -13,4 +13,29 @@ describe('CalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should guarantee that 1 + 4 = 5', () => {
+    const sum = service.calculate(1, 4, CalculatorService.PLUS);
+    expect(sum).toEqual(5);
+  });
+
+  it('should guarantee that 1 - 4 = -3', () => {
+    const sum = service.calculate(1, 4, CalculatorService.MINUS);
+    expect(sum).toEqual(-3);
+  });
+
+  it('should guarantee that 1 / 4 = 0.25', () => {
+    const sum = service.calculate(1, 4, CalculatorService.DIVIDE);
+    expect(sum).toEqual(0.25);
+  });
+
+  it('should guarantee that 1 * 4 = 4', () => {
+    const sum = service.calculate(1, 4, CalculatorService.TIMES);
+    expect(sum).toEqual(4);
+  });
+
+  it('should return 0 to invalid operation', () => {
+    const sum = service.calculate(1, 4, '%');
+    expect(sum).toEqual(0);
+  });
 });
