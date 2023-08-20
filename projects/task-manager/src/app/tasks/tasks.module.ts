a0c606551ca03e312008e03795c28921f3a9cf2c
Carlos Leão <caikleao@gmail.com>
2023-08-20 15:48:44 -0300

    Create done task directive

--- projects/task-manager/src/app/tasks/tasks.module.ts
+++ projects/task-manager/src/app/tasks/tasks.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { TaskService } from './shared';
+import { TaskService, DoneTaskDirective } from './shared';
 import { ListTaskComponent } from './list';
 import { InsertTaskComponent } from './insert/insert-task.component';
 import { EditTaskComponent } from './edit/edit-task.component';
 
 @NgModule({
-  declarations: [ListTaskComponent, InsertTaskComponent, EditTaskComponent],
+  declarations: [
+    ListTaskComponent,
+    InsertTaskComponent,
+    EditTaskComponent,
+    DoneTaskDirective,
+  ],
   imports: [CommonModule, RouterModule, FormsModule],
   providers: [TaskService],
 })
