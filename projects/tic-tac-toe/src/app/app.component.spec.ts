769c4cfd9a4044166c6ad787134169274685f232
Carlos Leão <caikleao@gmail.com>
2023-08-21 09:35:25 -0300

    Add tic-tac-toe module

--- projects/tic-tac-toe/src/app/app.component.spec.ts
+++ projects/tic-tac-toe/src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { TicTacToeModule } from './tic-tac-toe';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [TicTacToeModule],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -22,6 +27,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tic-tac-toe app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'tic-tac-toe app is running!'
+    );
   });
 });
