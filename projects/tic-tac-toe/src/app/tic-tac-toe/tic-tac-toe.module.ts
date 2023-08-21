18988fc912af4c3bb8d6091476ffd4fc9e06aefb
Carlos Leão <caikleao@gmail.com>
2023-08-21 09:40:13 -0300

    Create tic-tac-toe service

--- projects/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ projects/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { TicTacToeComponent } from './tic-tac-toe.component';
+import { TicTacToeService } from './shared';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TicTacToeComponent],
+  imports: [CommonModule],
+  exports: [TicTacToeComponent],
+  providers: [TicTacToeService],
 })
-export class TicTacToeModule { }
+export class TicTacToeModule {}
