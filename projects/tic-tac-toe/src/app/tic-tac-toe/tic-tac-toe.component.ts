6665cea7d0d7f68508cfc58ada04d12fdfb1b32d
Carlos Leão <caikleao@gmail.com>
2023-08-21 10:27:38 -0300

    Add new features to start page

--- projects/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ projects/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TicTacToeService } from './shared';
 
 @Component({
   selector: 'app-tic-tac-toe',
   templateUrl: './tic-tac-toe.component.html',
-  styleUrls: ['./tic-tac-toe.component.css']
+  styleUrls: ['./tic-tac-toe.component.css'],
 })
-export class TicTacToeComponent {
+export class TicTacToeComponent implements OnInit {
+  constructor(private ticTacToeService: TicTacToeService) {}
 
+  ngOnInit(): void {
+    this.ticTacToeService.initialize();
+  }
+
+  get showStart(): boolean {
+    return this.ticTacToeService.showStart;
+  }
+
+  get showBoard(): boolean {
+    return this.ticTacToeService.showBoard;
+  }
+
+  get showEnd(): boolean {
+    return this.ticTacToeService.showEnd;
+  }
+
+  startGame(): void {
+    this.ticTacToeService.startGame();
+  }
 }
