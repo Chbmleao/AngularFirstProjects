aa17bf6e33e8dcd3acb8ee1fa5e4a4e60f93e2ec
Carlos Leão <caikleao@gmail.com>
2023-08-22 10:24:07 -0300

    Implement the dashboard data service

--- projects/dashboard/src/app/dashboard/data.service.ts
+++ projects/dashboard/src/app/dashboard/data.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  readonly data = [
+    ['January', 33],
+    ['February', 68],
+    ['March', 49],
+    ['April', 15],
+    ['May', 80],
+    ['June', 27],
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getData(): Observable<any> {
+    return new Observable((observable) => {
+      observable.next(this.data);
+      observable.complete();
+    });
+  }
 }
