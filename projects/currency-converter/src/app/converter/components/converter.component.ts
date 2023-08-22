aa43c54300a194d5c12764d4691faea3eac3b730
Carlos Leão <caikleao@gmail.com>
2023-08-21 22:53:56 -0300

    Implement the currency converter form

--- projects/currency-converter/src/app/converter/components/converter.component.ts
+++ projects/currency-converter/src/app/converter/components/converter.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Currency, Conversion, ConversionResponse } from '../models';
+import { CurrencyService, ConverterService } from '../services';
 
 @Component({
   selector: 'app-converter',
   templateUrl: './converter.component.html',
-  styleUrls: ['./converter.component.css']
+  styleUrls: ['./converter.component.css'],
 })
-export class ConverterComponent {
+export class ConverterComponent implements OnInit {
+  currencies: Currency[];
+  conversion: Conversion;
+  hasError: boolean;
+  conversionResponse: ConversionResponse;
 
+  @ViewChild('conversionForm', { static: true }) conversionForm: NgForm;
+
+  constructor(
+    private currencyService: CurrencyService,
+    private converterService: ConverterService
+  ) {}
+
+  ngOnInit(): void {
+    this.currencies = this.currencyService.getAll();
+    this.init();
+  }
+
+  init() {
+    this.conversion = new Conversion('USD', 'BRL', null);
+    this.hasError = false;
+  }
+
+  convert() {
+    if (this.conversionForm.form.valid) {
+      alert('Converting: ' + JSON.stringify(this.conversion));
+    }
+  }
 }
--- projects/currency-converter/src/app/converter/converter.module.ts
+++ projects/currency-converter/src/app/converter/converter.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ConverterComponent } from './components';
 import { CurrencyService, ConverterService } from './services';
 
 @NgModule({
   declarations: [ConverterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [ConverterComponent],
   providers: [CurrencyService, ConverterService],
 })
