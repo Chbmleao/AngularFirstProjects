874ea4cf1525254e541b3766d99d6a1d73445974
Carlos Leão <caikleao@gmail.com>
2023-08-22 10:19:39 -0300

    Create the dashboard data service

--- projects/dashboard/src/app/dashboard/dashboard.component.spec.ts
+++ projects/dashboard/src/app/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { DataService } from './data.service';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +9,8 @@ describe('DashboardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DashboardComponent]
+      declarations: [DashboardComponent],
+      providers: [DataService],
     });
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
