f811f26bbd9c36d07d9a20bfd8b01f7fc6ec0877
Carlos Leão <caikleao@gmail.com>
2023-08-22 00:22:52 -0300

    Implement the date format pipe

--- projects/currency-converter/src/app/converter/pipes/br-date.pipe.spec.ts
+++ projects/currency-converter/src/app/converter/pipes/br-date.pipe.spec.ts
@@ -5,4 +5,9 @@ describe('BrDatePipe', () => {
     const pipe = new BrDatePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should format the date 2017-03-31 to 31/03/2017', () => {
+    const pipe = new BrDatePipe();
+    expect(pipe.transform('2017-03-31')).toEqual('31/03/2017');
+  });
 });
--- projects/currency-converter/src/app/converter/pipes/br-date.pipe.ts
+++ projects/currency-converter/src/app/converter/pipes/br-date.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'brDate'
+  name: 'brDate',
 })
 export class BrDatePipe implements PipeTransform {
+  transform(usDate: string): string {
+    if (!usDate) {
+      return '';
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    const dateArr = usDate.split('-');
+
+    if (dateArr.length !== 3) {
+      return usDate;
+    }
+
+    return dateArr[2] + '/' + dateArr[1] + '/' + dateArr[0];
   }
-
 }
