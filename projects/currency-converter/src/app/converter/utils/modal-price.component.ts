294e7dfe61e923abbda58e77dae943d92993955a
Carlos Leão <caikleao@gmail.com>
2023-08-21 23:30:18 -0300

    Create the modal price component

---
+++ projects/currency-converter/src/app/converter/utils/modal-price.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modal-price',
+  templateUrl: './modal-price.component.html',
+  styleUrls: ['./modal-price.component.css'],
+})
+export class ModalPriceComponent {}
